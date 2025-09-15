43b9b5fdc8dad2b275403449a04a10f36842105e
VladMajstat <vladmajstat1@gmail.com>
2025-09-15 11:54:21 +0300

    version_010_registration form and controler are ready

--- users/views.py
+++ users/views.py
@@ -2,43 +2,58 @@ from django.shortcuts import render
 from django.contrib import auth
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 def login(request):
+    # Якщо запит POST — обробляємо дані форми
     if request.method == 'POST':
+        # Створюємо форму з даними, які надіслав користувач
         form = UserLoginForm(data=request.POST)
+        # Перевіряємо, чи форма валідна (логін і пароль коректні)
         if form.is_valid():
             username = request.POST['username']
             password = request.POST['password']
+            # Аутентифікуємо користувача
             user = auth.authenticate(username=username, password=password)
             if user:
+                # Якщо користувач знайдений — виконуємо вхід і редірект на головну
                 auth.login(request, user)
                 return HttpResponseRedirect(reverse('main:index'))
     else:
+        # Якщо GET-запит — створюємо порожню форму
         form = UserLoginForm()
 
-    content = {
+    # Передаємо форму у шаблон для відображення
+    context = {
         'title': 'Авторизація - Binify',
         'form': form,
     }
-    return render(request, 'users/login.html', content)
+    return render(request, 'users/login.html', context)
 
 def registration(request):
-    content = {
-        'title': 'Реєстрація - Binify'
+
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserRegistrationForm()
+
+    context = {
+        'title': 'Реєстрація - Binify',
+        'form':form,
     }
-    return render(request, 'users/registration.html', content)
+    return render(request, 'users/registration.html', context)
 
 def profile(request):
-    content = {
+    context = {
         'title': 'Профіль - Binify'
     }
-    return render(request, 'users/profile.html', content)
+    return render(request, 'users/profile.html', context)
 
 def logout(request):
-    # content = {
-    #     'title': 'Вихід - Binify'
-    # }
-    # return render(request, 'users/logout.html', content)
-    
-    pass
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('main:index'))
